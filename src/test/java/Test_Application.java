874b1deda9468ffb04696e35f77804bb001e2a97
malar <malarchandar@gmail.com>
2020-12-22 18:46:52 +0000

    commiting my first git

---
+++ src/test/java/Test_Application.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+public class Test_Application {
+
+
+ @Test
+
+    public void test1(){
+     System.out.println("running test1");
+
+ }
+
+
+
+    @Test
+
+    public void test2(){
+        System.out.println("running test2");
+
+    }
+
+    @Test
+
+    public void test3(){
+        System.out.println("running test3");
+
+    }
+
+    @Test
+
+    public void test4(){
+        System.out.println("running test4");
+
+    }
+
+    @Test
+
+    public void test5(){
+        System.out.println("running test5");
+
+    }
+
+}
+
+
+
